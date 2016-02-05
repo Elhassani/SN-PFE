b3ca7fbdf82e0c196750e96968dd4a7bf7e8c7bc
Amine Elouattar <elouattar@gmail.com>
2016-02-05 14:47:56 +0000

    Commit1
    Ajout de 3 bouton pour travaillé sur le meme projet

--- app/src/main/java/com/example/amineelouattar/pfe/MainActivity.java
+++ app/src/main/java/com/example/amineelouattar/pfe/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.amineelouattar.pfe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btna, btnb, btnc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btna = (Button) findViewById(R.id.grpa);
+        btnb = (Button) findViewById(R.id.grpb);
+        btnc = (Button) findViewById(R.id.grpc);
+
+        btnc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FindServices.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
 }
